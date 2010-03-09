4734797e5c46ed8ae40bcf5553ea7dfa380cc060
Sebastian Mayer <sebioff@gmail.com>
2010-03-09 13:13:43 +0000

    - fixed a migration

--- migrations/020.constraints.php
+++ migrations/020.constraints.php
@@ -2,16 +2,4 @@
 
 $queries[] = 'ALTER TABLE `shoutbox_alliances` DROP FOREIGN KEY `shoutbox_alliances_ibfk_3` ;';
 
-$queries[] = 'ALTER TABLE `shoutbox_alliances` ADD FOREIGN KEY ( `alliance` ) REFERENCES `rakuun_3_live`.`alliances` (`id`) ON DELETE CASCADE ;';
-
-$queries[] = 'ALTER TABLE `shoutbox_alliances` DROP FOREIGN KEY `shoutbox_alliances_ibfk_2` ;';
-
-$queries[] = 'ALTER TABLE `shoutbox_metas` DROP FOREIGN KEY `shoutbox_metas_ibfk_2` ;';
-
-$queries[] = 'ALTER TABLE `boards_alliance_postings` DROP FOREIGN KEY `boards_alliance_postings_ibfk_2` ;';
-
-$queries[] = 'ALTER TABLE `boards_meta_postings` DROP FOREIGN KEY `boards_meta_postings_ibfk_2` ;';
-
-$queries[] = 'ALTER TABLE `boards_postings` DROP FOREIGN KEY `boards_postings_ibfk_2` ;';
-
 ?>
