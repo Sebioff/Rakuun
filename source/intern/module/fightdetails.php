f1afb2cb51182cd05d5c4789f4a95adda74068ad
Sebastian Mayer <sebioff@gmail.com>
2010-03-09 14:04:27 +0000

    - fixed information leak in fight details module (wasn't live yet)

--- source/intern/module/fightdetails.php
+++ source/intern/module/fightdetails.php
@@ -9,7 +9,7 @@ class Rakuun_Intern_Module_FightDetails extends Rakuun_Intern_Module {
 		
 		$options = array();
 		$options['conditions'][] = array('user = ?', Rakuun_User_Manager::getCurrentUser());
-		$army = Rakuun_DB_Containers::getArmiesContainer()->selectByPK($this->getParam('id'));
+		$army = Rakuun_DB_Containers::getArmiesContainer()->selectByPK($this->getParam('id'), $options);
 		if ($army)
 			$this->contentPanel->addPanel(new Rakuun_GUI_Panel_Box('details', new Rakuun_Intern_GUI_Panel_Map_Fights_Details('details', $army), 'Kampfdetails'));
 	}
