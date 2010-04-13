63878963cf789880f355656daea4ac624766c99e
Sebastian Mayer <sebioff@gmail.com>
2010-04-13 11:00:54 +0000

    - reverted change introduced in rev597 regarding speed reduction factor of effects that increase an armies speeed (was correct before)

--- source/intern/gui/panel/polls/polls.php
+++ source/intern/gui/panel/polls/polls.php
@@ -17,6 +17,7 @@ class Rakuun_Intern_GUI_Panel_Polls extends GUI_Panel {
 		parent::init();
 		
 		$this->setTemplate(dirname(__FILE__).'/polls.tpl');
+		$options = array();
 		$options['order'] = 'date DESC';
 		$options['conditions'][] = array('type = ?', $this->polltype);
 		if ($this->polltype == self::POLL_ALLIANCE)
