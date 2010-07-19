765b42c76e419bd0149115f00703d8e41dcd0c85
Sebastian Mayer <sebioff@gmail.com>
2010-07-19 13:28:05 +0000

    - added explanatory tooltip to "save capacity" on ressources page (#472)

--- source/intern/gui/panel/ressources/capacity.php
+++ source/intern/gui/panel/ressources/capacity.php
@@ -21,7 +21,7 @@ class Rakuun_Intern_GUI_Panel_Ressources_Capacity extends GUI_Panel {
 		
 		$ressourceProductionTable->addLine(
 			array(
-				'Sichere Kapazität',
+				new Rakuun_GUI_Panel_Info('save_capacity', 'Sichere Kapazität', 'Die sichere Kapazität gibt an, bis zu welcher Grenze Ressourcen gestohlen werden können.<br/>Der Wert steigt um '.GUI_Panel_Number::formatNumber(Rakuun_Intern_Production_Building_Store::SAVE_CAPACITY_RAISE_PER_WEEK * RAKUUN_STORE_CAPACITY_SAVE_MULTIPLIER).' pro Woche.'),
 				GUI_Panel_Number::formatNumber(Rakuun_User_Manager::getCurrentUser()->ressources->getSaveCapacityIron()),
 				GUI_Panel_Number::formatNumber(Rakuun_User_Manager::getCurrentUser()->ressources->getSaveCapacityBeryllium()),
 				GUI_Panel_Number::formatNumber(Rakuun_User_Manager::getCurrentUser()->ressources->getSaveCapacityEnergy()),
