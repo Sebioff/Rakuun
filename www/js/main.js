423201adaa7d1d02da4cee0cf6a30b5ecfacfb81
Sebastian Mayer <sebioff@gmail.com>
2010-07-25 13:26:31 +0000

    - improved rendering of collapsed boxes: previously the content of collapsed boxes has been visible while the page was loading and was only hidden after the JS has been executed (resulting in the pages content moving around while it is being loaded); now collapsed boxes are instantly displayed as collapsed

--- www/js/main.js
+++ www/js/main.js
@@ -1,37 +1,44 @@
 function GUI_Control_Box_Collapsible(collapseStatusScriptletURL, controlID) {
-	var height = $("#" + controlID + " .content").height();
-	var animationDuration = height * 8;
-	
 	$("#" + controlID + " .head").click(
 		function() {
-			$.get(collapseStatusScriptletURL, { panel: controlID });
+			var element = $("#" + controlID);
 			
-			var contentInner = $(this).parent().find(".content_inner");
-			var content = $(this).parent().find(".content");
-
-			if (content.height() > 0) {
-				content.css({overflow: "hidden"});
-				contentInner.animate( {top: "-" + (height + 4) + "px"}, { queue:false, duration: animationDuration});
-				content.animate( {height: "0px"}, { queue:false, duration: animationDuration, 
-					complete: function() {
-						$("#" + controlID).addClass("collapsed");
-					}
-				});
-			}
-			else {
-				contentInner.animate( {top: "0px"}, { queue:false, duration: animationDuration });
-				content.animate( {height: height + "px"}, { queue:false, duration: animationDuration, 
-					complete: function() {
-						$(this).css({overflow: "auto"});
-						$("#" + controlID).removeClass("collapsed");
-					}
-				});
+			if (!element.hasClass("animating")) {
+				element.addClass("animating");
+				$.get(collapseStatusScriptletURL, { panel: controlID });
+				var contentInner = element.find(".content_inner");
+				var content = element.find(".content");
+				
+				if (!element.hasClass('collapsed')) {
+					content.css({overflow: "hidden"});
+					height = content.height();
+					animationDuration = height * 8;
+					contentInner.animate( {top: "-" + (height + 4) + "px"}, { queue:false, duration: animationDuration});
+					content.animate( {height: "0px"}, { queue:false, duration: animationDuration, 
+						complete: function() {
+							element.addClass("collapsed");
+							$(this).css({height: ""});
+							element.removeClass("animating");
+						}
+					});
+				}
+				else {
+					contentInner.css({top: "-1000000px"});
+					element.removeClass("collapsed");
+					height = content.height();
+					animationDuration = height * 8;
+					content.css({height: "0px", overflow: "hidden"});
+					contentInner.css({top: "-" + (height + 4) + "px"});
+					
+					contentInner.animate( {top: "0px"}, { queue:false, duration: animationDuration });
+					content.animate( {height: height + "px"}, { queue:false, duration: animationDuration, 
+						complete: function() {
+							$(this).css({overflow: "auto"});
+							element.removeClass("animating");
+						}
+					});
+				}
 			}
 		}
 	);
-	
-	if ($("#" + controlID).hasClass("collapsed")) {
-		$("#" + controlID + " .content").css({height: "0px", overflow: "hidden"});
-		$("#" + controlID + " .content_inner").css({top: "-" + (height + 4) + "px"});
-	}
 }
