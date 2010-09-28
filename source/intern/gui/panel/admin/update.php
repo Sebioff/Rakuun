64f1e0fe2d64e533d2e46b19fceb3bb274838f3d
Sebastian Mayer <sebioff@gmail.com>
2010-09-28 14:41:02 +0000

    - modified update process; newstext can be changed, integrated twitter

--- source/intern/gui/panel/admin/update.php
+++ source/intern/gui/panel/admin/update.php
@@ -1,25 +1,75 @@
 <?php
 
 require_once dirname(__FILE__).'/3rdparty/Net/SSH2.php';
+require_once dirname(__FILE__).'/3rdparty/class.twitter.php';
 
 class Rakuun_Intern_GUI_Panel_Admin_Update extends GUI_Panel {
+	const STATE_PREPARING = 1;
+	const STATE_REVIEWING = 2;
+	
 	public function __construct($name) {
 		parent::__construct($name);
 		
 		$this->setTemplate(dirname(__FILE__).'/update.tpl');
 		$this->addPanel(new GUI_Control_CheckBox('create_news', '', true, 'Newseintrag erstellen'));
-		$this->addPanel(new GUI_Control_SubmitButton('update', 'Spiel aktualisieren'));
+		$this->addPanel(new GUI_Control_SubmitButton('update', 'Vorbereiten...'));
+		$this->addPanel($state = new GUI_Control_HiddenBox('state', self::STATE_PREPARING));
 		$this->addPanel(new GUI_Panel_Text('update_log', '', 'Updatelog'));
 	}
 	
 	public function onUpdate() {
-		$ssh = new Net_SSH2(RAKUUN_SSH_ADDRESS);
-		if (!$ssh->login(RAKUUN_SSH_USER, RAKUUN_SSH_PASSWORD)) {
-			$this->addError('SSH login failed');
+//		$ssh = new Net_SSH2(RAKUUN_SSH_ADDRESS);
+//		if (!$ssh->login(RAKUUN_SSH_USER, RAKUUN_SSH_PASSWORD)) {
+//			$this->addError('SSH login failed');
+//		}
+		
+		if ($this->hasErrors()) {
+			$this->state->setValue(self::STATE_PREPARING);
+			$this->update->setValue('Vorbereiten...');
+			return;
+		}
+		else {
+			if ($this->createNews->getSelected()) {
+				// FIXME hardcoded paths suck!
+				$history = explode("\n", $ssh->exec('svn log www/Rakuun -r '.$revisionNumber.':'.(PROJECT_VERSION + 1)));
+				$formatHistory = $this->formatHistory($history);
+				$this->addPanel(new GUI_Control_TextArea('newstext', 'Das Spiel wurde gerade aktualisiert. Folgende Änderungen wurden vorgenommen:\n<br>\n<br>\n'.$formatHistory, 'Newseintrag'));
+				$this->addPanel($tweet = new GUI_Control_TextArea('tweet', 'Update: '.$formatHistory, 'Tweet'));
+				$tweet->setAttribute('rows', 5);
+				// FIXME remove duplicated code, copied from shoutbox
+				$tweet->addJS(sprintf(
+					'
+						function shoutboxCountCharactersDown() {
+							if ($("#%1$s").val().length > %2$d)
+								$("#%1$s").val($("#%1$s").val().substring(0, %2$d));
+							$("#shoutbox_characters_left").val(%2$d - $("#%1$s").val().length);
+						}
+						
+						$("#%1$s").keypress(function() {
+							shoutboxCountCharactersDown();
+						}).keyup(function() {
+							shoutboxCountCharactersDown();
+						}).change(function() {
+							shoutboxCountCharactersDown();
+						});
+					',
+					$tweet->getID(), 140
+				));
+			}
 		}
 		
-		if ($this->hasErrors())
+		if ($this->state->getValue() == self::STATE_PREPARING) {
+			$this->state->setValue(self::STATE_REVIEWING);
+			$this->update->setValue('Spiel aktualisieren');
 			return;
+		}
+		else {
+			$this->state->setValue(self::STATE_PREPARING);
+		}
+		
+		dump($this->newstext->getValue());
+		
+		return;
 			
 		$log = '';
 		
@@ -81,11 +131,17 @@ class Rakuun_Intern_GUI_Panel_Admin_Update extends GUI_Panel {
 			$formatHistory = $this->formatHistory($history);
 			
 			// add news entry
-			$newsEntry = new DB_Record();
-			$newsEntry->subject = 'Update';
-			$newsEntry->text = 'Das Spiel wurde gerade aktualisiert. Folgende Änderungen wurden vorgenommen:<br/><br/>'.$formatHistory;
-			$newsEntry->time = time();
-			Rakuun_DB_Containers_Persistent::getNewsContainer()->save($newsEntry);
+			if ($this->newstext->getValue()) {
+				$newsEntry = new DB_Record();
+				$newsEntry->subject = 'Update';
+				$newsEntry->text = $this->newstext->getValue();
+				$newsEntry->time = time();
+				Rakuun_DB_Containers_Persistent::getNewsContainer()->save($newsEntry);
+			}
+			if ($this->twitter->getValue()) {
+				$object = new twitter(RAKUUN_TWITTER_USER, RAKUUN_TWITTER_PASSWORD);
+				$object->post_tweet($this->twitter->getValue());
+			}
 		}
 		
 		// update revision number
--- source/intern/gui/panel/admin/update.tpl
+++ source/intern/gui/panel/admin/update.tpl
@@ -1,11 +1,24 @@
 <? if ($this->hasErrors()): ?>
-	<? $this->displayErrors() ?>
+	<? $this->displayErrors(); ?>
 <? endif; ?>
-<? $this->displayPanel('create_news') ?>
-<? $this->displayPanel('update') ?>
+<? $this->displayPanel('create_news'); ?>
+<? if ($this->hasPanel('newstext')): ?>
+	<br class="clear"/>
+	<? $this->displayLabelForPanel('newstext'); ?> <? $this->displayPanel('newstext'); ?>
+	<br class="clear"/>
+	<? $this->displayLabelForPanel('tweet'); ?> <? $this->displayPanel('tweet'); ?>
+	<br class="clear"/>
+	<span style="float:left">Buchstaben übrig:</span> <input id="shoutbox_characters_left" type="text" size="3" value="140" readonly="readonly" />
+<? elseif($this->state->getValue() == Rakuun_Intern_GUI_Panel_Admin_Update::STATE_REVIEWING): ?>
+	<br class="clear"/>
+	Es wird kein Newseintrag erstellt.
+<? endif; ?>
+<? $this->displayPanel('update'); ?>
 <br />
 <? if (isset($this->params->conflicts) && $this->params->conflicts): ?>
 	<span style="color:#ff0000; font-weight:bold">Es gab Konflikte beim Updaten!</span>
 	<br/>
 <? endif; ?>
-<? $this->displayPanel('update_log') ?>
+<? $this->displayPanel('update_log'); ?>
+<? $this->displayPanel('state'); ?>
+<br class="clear"/>
