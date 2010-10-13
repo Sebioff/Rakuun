4209de71ccf0a5b3e81ebadc913b3128c273340c
Andre Jährling <dr.dent@gmx.net>
2010-10-13 10:58:31 +0000

    - more tablefixes with hostname datatype

--- migrations/003.log_user_activity_hostname_fix.php
+++ migrations/003.log_user_activity_hostname_fix.php
@@ -1,5 +1,7 @@
 <?php
 
-$queries[] = 'ALTER TABLE `log_users_activity` CHANGE `hostname` `hostname` VARCHAR( 60 ) NOT NULL';
+$queries[] = 'ALTER TABLE `log_users_activity` CHANGE `hostname` `hostname` VARCHAR( 60 ) NOT NULL;';
+$queries[] = 'ALTER TABLE `log_users_ressourcetransfer` CHANGE `hostname` `hostname` VARCHAR( 60 ) NOT NULL;';
+$queries[] = 'ALTER TABLE `log_users_data` CHANGE `hostname` `hostname` VARCHAR( 60 ) NOT NULL;';
 
 ?>
