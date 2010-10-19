de0f346837ca5494a0411d118cb161a2d5990b99
Andre Jährling <dr.dent@gmx.net>
2010-10-19 21:52:31 +0000

    - added collum action to multiactions and renamed old action to 'übereinstimmung'

--- source/intern/gui/panel/multihunting/multiactionlog.php
+++ source/intern/gui/panel/multihunting/multiactionlog.php
@@ -18,13 +18,13 @@ class Rakuun_Intern_GUI_Panel_Multihunting_MultiActionLog extends GUI_Panel {
 		$this->setTemplate(dirname(__FILE__).'/multiactionlog.tpl');
 		if ($this->user) {
 			$this->addPanel($log = new GUI_Panel_Table('log', 'Multi-Aktivität'));
-			$log->addHeader(array('Zeit', 'User', 'Aktion'));
+			$log->addHeader(array('Zeit', 'User', 'Übereinstimmung', 'Aktion'));
 			$i = 0;
 			foreach (Rakuun_DB_Containers::getLogMultiactionsUsersAssocContainer()->selectByUser($this->user) as $activity) {
 				foreach (Rakuun_DB_Containers::getLogMultiactionsUsersAssocContainer()->selectByMultiAction($activity->multiAction) as $action) {
 					if ($activity->user != $action->user) {
 						$date = new GUI_Panel_Date('date'.$i++, $action->multiAction->time);
-						$log->addLine(array($date, $action->user->name, Rakuun_Intern_Log::getMultiActionDescription($action->multiAction->multiaction)));
+						$log->addLine(array($date, $action->user->name, Rakuun_Intern_Log::getMultiActionDescription($action->multiAction->multiaction), Rakuun_Intern_Log::getActionDescription($action->multiAction->action)));
 					}
 				}
 			}
