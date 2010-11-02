41c018de8b52eb4ca69ec70eed4a086b61a0fdcc
Sebastian Mayer <sebioff@gmail.com>
2010-11-02 18:02:21 +0000

    - removed unnecessary template

--- source/intern/module/fightdetails.php
+++ source/intern/module/fightdetails.php
@@ -5,15 +5,12 @@ class Rakuun_Intern_Module_FightDetails extends Rakuun_Intern_Module {
 		parent::init();
 		
 		$this->setPageTitle('Kampfinformationen');
-		$this->contentPanel->setTemplate(dirname(__FILE__).'/fightdetails.tpl');
 		
 		$options = array();
 		$options['conditions'][] = array('user = ?', Rakuun_User_Manager::getCurrentUser());
 		$army = Rakuun_DB_Containers::getArmiesContainer()->selectByPK($this->getParam('id'), $options);
-		if ($army) {
-			$this->contentPanel->setTemplate(dirname(__FILE__).'/fightdetails.tpl');	
+		if ($army)
 			$this->contentPanel->addPanel(new Rakuun_GUI_Panel_Box('details', new Rakuun_Intern_GUI_Panel_Map_Fights_Details('details', $army), 'Kampfdetails'));
-		}
 	}
 }
 
--- source/intern/module/fightdetails.tpl
+++
@@ -1,1 +0,0 @@
-<? $this->displayPanel('details'); ?>
