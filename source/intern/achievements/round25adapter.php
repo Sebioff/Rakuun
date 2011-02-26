e6aa9d37d3b1e5d11c4b1686490273eb795b9607
Sebastian Mayer <sebioff@gmail.com>
2011-02-26 17:35:09 +0000

    - more of the same

--- source/intern/achievements/round25adapter.php
+++ source/intern/achievements/round25adapter.php
@@ -5,8 +5,8 @@ class Rakuun_Intern_Achievements_Round25Adapter extends Rakuun_Intern_Achievemen
 		parent::addAchievements($user, $eternalUser, $roundName);
 		
 		// member of winning meta
-		$alliance = Rakuun_DB_Containers_Persistent::getAlliancesContainer()->selectByPK($user->alliance);
-		if ($alliance) {
+		if ($user->alliance) {
+			$alliance = Rakuun_DB_Containers_Persistent::getAlliancesContainer()->selectByIDFirst($user->alliance);
 			$meta = Rakuun_DB_Containers_Persistent::getMetasContainer()->selectByPK($alliance->meta);
 			if ($meta && $meta->name == $this->getRoundInformation($roundName)->winningMeta)
 				$this->saveAchievement($eternalUser, $roundName, 'Mitglied der Siegermeta');
