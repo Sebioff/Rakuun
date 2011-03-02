3b365c884b631ed0caf414c45a993a8c19465372
Andre Jährling <dr.dent@gmx.net>
2011-03-02 19:59:41 +0000

    - fix non-property exception

--- source/intern/module/showprofile.php
+++ source/intern/module/showprofile.php
@@ -13,9 +13,9 @@ class Rakuun_Intern_Module_ShowProfile extends Rakuun_Intern_Module {
 		
 		$param = $this->getParam('user');
 		$user = Rakuun_DB_Containers::getUserContainer()->selectByPK($param);
-		$this->contentPanel->addPanel($profileBox = new Rakuun_GUI_Panel_Box('showprofile', new Rakuun_Intern_GUI_Panel_User_ShowProfile('showprofile', $user), 'Profil von '.$user->name));
-		$profileBox->addClasses('rakuun_user_profile_box');
 		if ($user && $user->getPK() != Rakuun_User_Manager::getCurrentUser()->getPK()) {
+			$this->contentPanel->addPanel($profileBox = new Rakuun_GUI_Panel_Box('showprofile', new Rakuun_Intern_GUI_Panel_User_ShowProfile('showprofile', $user), 'Profil von '.$user->name));
+			$profileBox->addClasses('rakuun_user_profile_box');
 			$this->contentPanel->addPanel(new Rakuun_GUI_Panel_Box('armybox', new Rakuun_Intern_GUI_Panel_Reports_Display_Army('army', $user), 'Einheitenübersicht'));
 			$this->contentPanel->addPanel(new Rakuun_GUI_Panel_Box('buildingsbox', new Rakuun_Intern_GUI_Panel_Reports_Display_Buildings('buildings', $user), 'Gebäudeübersicht'));
 			$this->contentPanel->addPanel($detailBox = new Rakuun_GUI_Panel_Box('detailsbox', null, 'Details'));
