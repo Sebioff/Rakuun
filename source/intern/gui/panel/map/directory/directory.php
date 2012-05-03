8602d4dfb0e518e700ae8b47a4785d295eefbe0b
Andre Jährling <dr.dent@gmx.net>
2012-05-03 16:15:18 +0000

    bugfix: $fight->opponent is not unique for deleted players

--- source/intern/gui/panel/map/directory/directory.php
+++ source/intern/gui/panel/map/directory/directory.php
@@ -7,11 +7,13 @@ abstract class Rakuun_Intern_GUI_Panel_Map_Directory extends GUI_Panel {
 		parent::init();
 		
 		$this->addPanel($list = new GUI_Panel_List('list'));
+		$i = 0;
 		foreach (Rakuun_DB_Containers::getLogOutgoingArmiesContainer()->select($this->options) as $fight) {
-			$panel = new GUI_Panel('links'.$fight->opponent);
+			$panel = new GUI_Panel('links_'.$i);
 			$panel->addPanel(new Rakuun_GUI_Control_MapLink('maplink', $fight->opponent));
 			$panel->addPanel(new Rakuun_GUI_Control_UserLink('userlink', $fight->opponent));
 			$list->addItem($panel);
+			$i++;
 		}
 	}
 }
