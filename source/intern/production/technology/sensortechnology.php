8799f1eab535466b5e0e94bd409d10d2a7199e56
Andre Jährling <dr.dent@gmx.net>
2012-07-01 18:30:43 +0000

    set right values for sensortechnology

--- source/intern/production/technology/sensortechnology.php
+++ source/intern/production/technology/sensortechnology.php
@@ -6,13 +6,13 @@ class Rakuun_Intern_Production_Technology_SensorTechnology extends Rakuun_Intern
 		
 		$this->setInternalName('sensor_technology');
 		$this->setName('Sensorik');
-		$this->setBaseIronCosts(20000);
-		$this->setBaseBerylliumCosts(30000);
-		$this->setBaseEnergyCosts(10000);
-		$this->setBasePeopleCosts(10000);
-		$this->setBaseTimeCosts(5*24*60*60);
-		$this->addNeededBuilding('laboratory', 10);
-		$this->setMaximumLevel(1);
+		$this->setBaseIronCosts(5000);
+		$this->setBaseBerylliumCosts(5000);
+		$this->setBaseEnergyCosts(5000);
+		$this->setBasePeopleCosts(1000);
+		$this->setBaseTimeCosts(24*60*60);
+		$this->addNeededBuilding('laboratory', 1);
+		$this->setMaximumLevel(4);
 		$this->setShortDescription('');
 		$this->setLongDescription('');
 		$this->setPoints(20);
